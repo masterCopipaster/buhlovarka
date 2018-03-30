b09413e87be5e47e820161f88c4901dc45090913
masterCopipaster <androror@gmail.com>
2018-03-30 11:19:55 +0300

    добавил зипок с драйвером для программатора

--- example/example.ino
+++ example/example.ino
@@ -25,14 +25,15 @@ void setup() {
 
 void loop() {
   lcd.setCursor(0, 0);
-  lcd.print("tick: ");
   lcd.print(millis() / 500); //откорректированное время (вместо 1000 стоит 500)
   //на четных реле выкл, на нечетных вкл
-  digitalWrite(7, (millis() / 500) & 1);
+  digitalWrite(RELAY_PIN, (millis() / 500) & 1);
+  if((millis() / 500) & 1) lcd.print("  RELAY ON  ");
+  else lcd.print("  RELAY OFF ");
   
   lcd.setCursor(0, 1);
   //чтение кнопки
-  if(!digitalRead(2)) lcd.print("button pressed"); // нажатая кнопка подтягивает вход к земле
+  if(!digitalRead(BUUTON_PIN)) lcd.print("button pressed"); // нажатая кнопка подтягивает вход к земле
   else lcd.print("                 "); // когда кнопка отпущена вход подтянут к 1
 }
 
